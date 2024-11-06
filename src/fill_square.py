3514e12184f96fcda2c4f6cbf48b262e534858e9
Dilan Patel <113666014+Diliano@users.noreply.github.com>
2024-11-06 15:15:22 +0000

    refactor fill_square() for readability and removing redundant conditionals

--- src/fill_square.py
+++ src/fill_square.py
@@ -1,26 +1,20 @@
 def fill_square(square):
     longest_row = max([len(row) for row in square])
     num_rows = len(square)
-    dimension = None
 
-    if longest_row > num_rows:
-        dimension = longest_row
-    else:
-        dimension = num_rows
+    dimension = longest_row if longest_row > num_rows else num_rows
 
     result = []
 
+    # build result using existing rows, checking for missing values
     for row in square:
-        if len(row) == dimension:
-            result.append(row)
-        else:
-            difference = dimension - len(row)
-            row.extend([None for _ in range(difference)])
-            result.append(row)
+        difference = dimension - len(row)
+        row.extend([None for _ in range(difference)])
+        result.append(row)
 
-    if len(result) < dimension:
-        difference = dimension - len(result)
-        for _ in range(difference):
-            result.append([None for _ in range(dimension)])
+    # build missing rows if required
+    difference = dimension - len(result)
+    for _ in range(difference):
+        result.append([None for _ in range(dimension)])
 
     return result
